0373ddbd0780d073f0dae85d5d9815cab0228d09
Tano Paul <tanopaul38@gmail.com>
2023-09-12 17:38:08 -0400

    completed python badges and schedules lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,20 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    list_of_badge_names = [f"Hello, my name is {name}." for name in names]
+    return list_of_badge_names
 
 def assign_rooms(names):
-    return None
+    list_of_rooms = [f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!" for name in names]
+    return list_of_rooms
 
 def printer(names):
-    return None
+    for i in range(len(names)):
+        print(badge_maker(names[i]))
+    print_rooms = assign_rooms(names)
+    for j in range(len(print_rooms)):
+        print(print_rooms[j])
+
+
+printer(['Tano', 'Rice', 'Andrada'])
